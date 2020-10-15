db471f0c892227d06004c8f193a9f7608587d5ba
Hristo <sandevh@gmail.com>
2020-10-15 12:55:53 -0400

    Added more insults
    We wanted to make the bot more like Elon

--- modules/solve.py
+++ modules/solve.py
@@ -2,6 +2,7 @@ import requests
 from os.path import join, dirname, os
 from dotenv import load_dotenv
 from xml.dom import minidom
+import random
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -9,6 +10,16 @@ TOKEN = os.getenv('WOLFRAM_TOKEN')
 
 serviceurl = "http://api.wolframalpha.com/v1/result?"
 
+pickReply = ["Wtf is this shit", 
+"Bruh lmao you're kidding me", 
+"Give me a joint instead of this shit", 
+"I'm depressed", 
+"Lol kill yourself", 
+"I can't solve this lmao", 
+"Aight im out"]
+
 def get_short_result(keyword):
     r = requests.get(serviceurl, params = {'appid': TOKEN, 'i': keyword})
+    if r.text == "Wolfram|Alpha did not understand your input":
+        return random.choice(pickReply)    
     return(r.text)
