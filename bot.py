d17402efe6d22c0a45a94ba79e91866129dccbac
itsjoeoui <idkwhoiam@itsjoeoui.com>
2020-10-15 16:33:35 -0400

    Add status checker
    * 3 shortcuts: mcserver, omnivox and moodle
    * Also clean up bot.py, add keyword variable
    
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from modules import address
 from modules import wolfram
 from modules import solve
+from modules import status
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -20,20 +21,26 @@ async def on_message(message):
         return
 
     wordlist = message.content.split()
+    keyword = ' '.join(wordlist[1:]) 
 
     if "bruh" in message.content.lower():
         await message.channel.send('Bruh')
 
     if "/address" == wordlist[0]:
-        await message.channel.send(address.get_full_address(" ".join(wordlist[1:])))
+        await message.channel.send(address.get_full_address(keyword))
 
     if "/wolfram" == wordlist[0]:
-        keyword = ' '.join(wordlist[1:]) 
         await message.channel.send(f'Searching "{keyword}" on WolframAlpha...')
         wolfram.get_full_result(keyword)
         await message.channel.send(file=discord.File('cache/wolfram_result.jpg', f'{keyword}.jpg'))
         
     if "/solve" == wordlist[0]:
-        await message.channel.send(solve.get_short_result(" ".join(wordlist[1:])))
+        await message.channel.send(solve.get_short_result(keyword))
+
+    if "/status" == wordlist[0]:
+        if status.get_status(keyword) == True:
+            await message.channel.send(f'"{keyword}" is currently up!')
+        else:
+            await message.channel.send(f'"{keyword}" is down or invalid :(') 
 
 client.run(TOKEN)
