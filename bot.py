86924227a91681f1918e0392e968bd78bbbae5b0
itsjoeoui <idkwhoiam@itsjoeoui.com>
2020-10-14 21:15:03 -0400

    Initial commit
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import discord
+from dotenv import load_dotenv
+from modules import geocoding
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    wordlist = message.content.split()
+
+    if "bruh" in message.content.lower():
+        await message.channel.send('Bruh')
+
+    if "/address" == wordlist[0]:
+        await message.channel.send(geocoding.getAddress(" ".join(wordlist[1:])))
+
+client.run(TOKEN)
---
+++ modules/geocoding.py
@@ -0,0 +1,40 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+def getAddress(address):
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        return(f"Can't find {address} in the database")
+
+    location = js['results'][0]['formatted_address']
+    return(location)
