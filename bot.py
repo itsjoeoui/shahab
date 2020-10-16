86c8bbace8338551f9855a2712c5118b5560f593
itsjoeoui <idkwhoiam@itsjoeoui.com>
2020-10-16 13:44:05 -0400

    Refactor bot.py
    * Use discord.ext.commands to handle all commands
    
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- bot.py
+++ bot.py
@@ -1,50 +1,58 @@
 import os
 import discord
 from dotenv import load_dotenv
-from modules import address
-from modules import wolfram
-from modules import solve
-from modules import status
+from modules import geocoding
+from modules import wolframalpha
+from modules import webstatus
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.command()
+async def address(ctx, *args):
+    keyword = ' '.join(args) 
+    await ctx.send(geocoding.get_full_address(keyword))
+
+@bot.command()
+async def wolfram(ctx, *args):
+    keyword = ' '.join(args) 
+    await ctx.send(f'Searching {keyword} on WolframAlpha...')
+    wolframalpha.get_full_result(keyword)
+    await ctx.send(file=discord.File('cache/wolfram_result.jpg', f'{keyword}.jpg'))
+
+@bot.command()
+async def solve(ctx, *args):
+    keyword = ' '.join(args) 
+    result = wolframalpha.get_short_result(keyword)
+    await ctx.send(wolframalpha.get_short_result(keyword))
+    if result == 'No short answer available':
+        await ctx.send('Retrieving a long answer...')
+        wolframalpha.get_full_result(keyword)
+        await ctx.send(file=discord.File('cache/wolfram_result.jpg', f'{keyword}.jpg'))
+
+@bot.command()
+async def status(ctx, *args):
+    keyword = ' '.join(args) 
+    if webstatus.get_status(keyword):
+        await ctx.send(f'{keyword} is currently up!')
+    else:
+        await ctx.send(f'{keyword} is down or invalid :(')
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
-
-    wordlist = message.content.split()
-    keyword = ' '.join(wordlist[1:])
 
     if "bruh" in message.content.lower():
         await message.channel.send('Bruh')
 
-    if wordlist[0] == "/address":
-        await message.channel.send(address.get_full_address(keyword))
+    await bot.process_commands(message)
 
-    if wordlist[0] == "/wolfram":
-        await message.channel.send(f'Searching "{keyword}" on WolframAlpha...')
-        wolfram.get_full_result(keyword)
-        await message.channel.send(file=discord.File('cache/wolfram_result.jpg', f'{keyword}.jpg'))
-
-    if wordlist[0] == "/solve":
-        result = solve.get_short_result(keyword)
-        await message.channel.send(solve.get_short_result(keyword))
-        if result == "No short answer available":
-            await message.channel.send("Retrieving a long answer...")
-            await message.channel.send(file=discord.File('cache/wolfram_result.jpg', f'{keyword}.jpg')) 
-
-    if wordlist[0] == "/status":
-        if status.get_status(keyword):
-            await message.channel.send(f'"{keyword}" is currently up!')
-        else:
-            await message.channel.send(f'"{keyword}" is down or invalid :(')
-
-client.run(TOKEN)
+bot.run(TOKEN)
