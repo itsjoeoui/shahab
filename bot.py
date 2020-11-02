f35e38aed48373d889bd10220d68fa67b10e9ab5
itsjoeoui <idkwhoiam@itsjoeoui.com>
2020-11-01 21:33:01 -0500

    You are very welcome
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- bot.py
+++ bot.py
@@ -57,6 +57,11 @@ async def on_message(message):
 
     if "bruh" in message.content.lower():
         await message.channel.send('Bruh')
+    
+    thanks_list = ['thank you', 'thank u', 'thanks', 'thx']
+
+    if any(x in message.content.lower() for x in thanks_list):
+        await message.channel.send(file=discord.File('audio/YouAreVeryWelcome.mp3'))
 
     await bot.process_commands(message)
 
