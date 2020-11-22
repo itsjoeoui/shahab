481b0c09e2cc280fa1c81fe1aaa23be7d7d7ee1b
itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>
2020-11-22 12:48:48 -0500

    Add unban command and notify if kick/ban/unban gets called
    Signed-off-by: itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>

--- bot.py
+++ bot.py
@@ -30,10 +30,23 @@ async def ping(ctx):
 @bot.command()
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
+    await ctx.send(f'Kicked {member.mention}')
 
 @bot.command()
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+@bot.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    name, discriminator = member.split('#')
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if (user.name, user.discriminator) == (name, discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
 
 @bot.command()
 async def address(ctx, *, args):
