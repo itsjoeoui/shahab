82b40f601f40054feb6b5a84a993a99cfc7e961c
itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>
2020-11-24 20:21:01 -0500

    [1/2] Refactor code
    * Move on to cogs
    * Remove status becuase it doesn't actually work
    
    Signed-off-by: itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>

--- bot.py
+++ bot.py
@@ -7,119 +7,35 @@ from modules import wolframalpha
 from modules import webstatus
 from discord.ext import commands
 
-RUN_STRINGS = (
-    "Where do you think you're going?",
-    "Huh? what? did they get away?",
-    "ZZzzZZzz... Huh? what? oh, just them again, nevermind.",
-    "Get back here!",
-    "Not so fast...",
-    "Look out for the wall!",
-    "Don't leave me alone with them!!",
-    "You run, you die.",
-    "Jokes on you, I'm everywhere",
-    "You're gonna regret that...",
-    "You could also try /kickme, I hear that's fun.",
-    "Go bother someone else, no-one here cares.",
-    "You can run, but you can't hide.",
-    "Is that all you've got?",
-    "I'm behind you...",
-    "You've got company!",
-    "We can do this the easy way, or the hard way.",
-    "You just don't get it, do you?",
-    "Yeah, you better run!",
-    "Please, remind me how much I care?",
-    "I'd run faster if I were you.",
-    "That's definitely the droid we're looking for.",
-    "May the odds be ever in your favour.",
-    "Famous last words.",
-    "And they disappeared forever, never to be seen again.",
-    "\"Oh, look at me! I'm so cool, I can run from a bot!\" - this person",
-    "Yeah yeah, just tap /kickme already.",
-    "Here, take this ring and head to Mordor while you're at it.",
-    "Legend has it, they're still running...",
-    "Unlike Harry Potter, your parents can't protect you from me.",
-    "Fear leads to anger. Anger leads to hate. Hate leads to suffering. If you keep running in fear, you might "
-    "be the next Vader.",
-    "Multiple calculations later, I have decided my interest in your shenanigans is exactly 0.",
-    "Legend has it, they're still running.",
-    "Keep it up, not sure we want you here anyway.",
-    "You're a wiza- Oh. Wait. You're not Harry, keep moving.",
-    "NO RUNNING IN THE HALLWAYS!",
-    "Hasta la vista, baby.",
-    "Who let the dogs out?",
-    "It's funny, because no one cares.",
-    "Ah, what a waste. I liked that one.",
-    "Frankly, my dear, I don't give a damn.",
-    "My milkshake brings all the boys to yard... So run faster!",
-    "You can't HANDLE the truth!",
-    "A long time ago, in a galaxy far far away... Someone would've cared about that. Not anymore though.",
-    "Hey, look at them! They're running from the inevitable banhammer... Cute.",
-    "Han shot first. So will I.",
-    "What are you running after, a white rabbit?",
-    "As The Doctor would say... RUN!",
-)
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='/')
+client = commands.Bot(command_prefix='/')
 
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Game('my Tesla /help'))
-    print('We have logged in as {0.user}'.format(bot))
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
-@bot.event
-async def on_member_join(member):
-    print(f'{member} has joined the server')
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
 
-@bot.event
-async def on_member_remove(member):
-    print(f'{member} has left the server')
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'Kicked {member.mention}')
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}')
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    name, discriminator = member.split('#')
-    for ban_entry in banned_users:
-        user = ban_entry.user
-        if (user.name, user.discriminator) == (name, discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}')
-            return
-
-@bot.command()
-async def runs(ctx):
-    await ctx.send(random.choice(RUN_STRINGS))
-
-@bot.command()
+# TDL: Move all commands below into cogs
+@client.command()
 async def address(ctx, *, args):
     await ctx.send(geocoding.get_full_address(args))
 
-@bot.command()
+@client.command()
 async def wolfram(ctx, *, args):
     await ctx.send(f'Searching {args} on WolframAlpha...')
     wolframalpha.get_full_result(args)
     await ctx.send(file=discord.File('cache/wolfram_result.jpg', f'{args}.jpg'))
 
-@bot.command()
+@client.command()
 async def solve(ctx, *, args):
     result = wolframalpha.get_short_result(args)
     await ctx.send(wolframalpha.get_short_result(args))
@@ -128,29 +44,22 @@ async def solve(ctx, *, args):
         wolframalpha.get_full_result(args)
         await ctx.send(file=discord.File('cache/wolfram_result.jpg', f'{args}.jpg'))
 
-@bot.command()
-async def status(ctx, *, args):
-    if webstatus.get_status(args):
-        await ctx.send(f'{args} is currently up!')
-    else:
-        await ctx.send(f'{args} is down or invalid :(')
-
-@bot.command()
+@client.command()
 async def say(ctx, *, args):
     await ctx.send(args)
 
-@bot.command()
+@client.command()
 async def source(ctx):
     await ctx.send('GitHub: https://github.com/itsjoeoui/discord')
 
-@bot.event
+@client.event
 async def on_message(message):
-    if message.author == bot.user:
+    if message.author == client.user:
         return
 
     if "bruh" in message.content.lower():
         await message.channel.send('Bruh')
 
-    await bot.process_commands(message)
+    await client.process_commands(message)
 
-bot.run(TOKEN)
+client.run(TOKEN)
---
+++ cogs/admin.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+
+class Admin(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.client.change_presence(activity=discord.Game('my Tesla /help'))
+        print('We have logged in as {0.user}'.format(self.client))
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f'{member} has joined the server')
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print(f'{member} has left the server')
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f'Kicked {member.mention}')
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f'Banned {member.mention}')
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        name, discriminator = member.split('#')
+        for ban_entry in banned_users:
+            user = ban_entry.user
+            if (user.name, user.discriminator) == (name, discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.mention}')
+                return
+
+def setup(client):
+    client.add_cog(Admin(client))
