94b83b5c8d92615c857a9758a773681b3ba36d31
Joey Yu <itsjoeoui@gmail.com>
2021-03-10 12:56:02 -0500

    Fixup

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 
 class Games(commands.Cog):
 
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.command()
     async def roll(self, ctx, *, side=6):
         """This rolls a D6 by default. However, you can specify any number of sides."""
