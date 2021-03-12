49cb594fa9f58e50320885532c96756b4c9dfc70
Joey Yu <itsjoeoui@gmail.com>
2021-03-11 22:08:59 -0500

    Switch to wavelink again

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -1,9 +1,6 @@
-import re
-
-import lavalink
+import discord
+import wavelink
 from discord.ext import commands
-
-url_rx = re.compile(r'https?://(?:www\.)?.+')
 
 class Music(commands.Cog):
 
@@ -12,90 +9,44 @@ class Music(commands.Cog):
         self.guild = bot.get_guild(754135767999185029)
         self.channel = bot.get_channel(819045649487626280)
 
-        if not hasattr(bot, 'lavalink'):  # This ensures the client isn't overwritten during cog reloads.
-            bot.lavalink = lavalink.Client(bot.user.id)
-            bot.lavalink.add_node('127.0.0.1', 2333, 'youshallnotpass', 'us', 'default-node')  # Host, Port, Password, Region, Name
-            bot.add_listener(bot.lavalink.voice_update_handler, 'on_socket_response')
+        if not hasattr(bot, 'wavelink'):
+            self.bot.wavelink = wavelink.Client(bot=self.bot)
 
-    def cog_unload(self):
-        """Cog unload handler. This removes any event hooks that were registered."""
-        self.bot.lavalink._event_hooks.clear()
+        self.bot.loop.create_task(self.start_nodes())
 
-    async def cog_before_invoke(self, ctx):
-        """Command before-invoke handler."""
-        guild_check = ctx.guild is not None
-        #  This is essentially the same as `@commands.guild_only()`
-        #  except it saves us repeating ourselves (and also a few lines).
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
 
-        if guild_check:
-            await self.ensure_voice(ctx)
-            #  Ensure that the bot and command author share a mutual voicechannel.
+        # Initiate our nodes. For this example we will use one server.
+        # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
+        await self.bot.wavelink.initiate_node(host='127.0.0.1',
+                                              port=2333,
+                                              rest_uri='http://127.0.0.1:2333',
+                                              password='youshallnotpass',
+                                              identifier='TEST',
+                                              region='us_central')
 
-        return guild_check
+    @commands.command(name='connect')
+    async def connect_(self, ctx):
+        player = self.bot.wavelink.get_player(self.guild.id)
+        await ctx.send(f'Connecting to **`{self.channel.name}`**')
+        await player.connect(self.channel.id)
 
-    async def cog_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.send(error.original)
-            # The above handles errors thrown in this cog and shows them to the user.
-            # This shouldn't be a problem as the only errors thrown in this cog are from `ensure_voice`
-            # which contain a reason string, such as "Join a voicechannel" etc. You can modify the above
-            # if you want to do things differently.
-
-    async def ensure_voice(self, ctx):
-        """This check ensures that the bot and command author are in the same voicechannel."""
-        player = self.bot.lavalink.player_manager.create(ctx.guild.id, endpoint=str(ctx.guild.region))
-        # Create returns a player if one exists, otherwise creates.
-        # This line is important because it ensures that a player always exists for a guild.
-
-        # Most people might consider this a waste of resources for guilds that aren't playing, but this is
-        # the easiest and simplest way of ensuring players are created.
-
-        # These are commands that require the bot to join a voicechannel (i.e. initiating playback).
-        # Commands such as volume/skip etc don't require the bot to be in a voicechannel so don't need listing here.
-        should_connect = ctx.command.name in ('setup',)
-
-        if not player.is_connected:
-            if not should_connect:
-                raise commands.CommandInvokeError('Not connected.')
-
-            # permissions = ctx.author.voice.channel.permissions_for(ctx.me)
-
-            # if not permissions.connect or not permissions.speak:  # Check user limit too?
-            #     raise commands.CommandInvokeError('I need the `CONNECT` and `SPEAK` permissions.')
-
-            player.store('channel', self.channel.id)
-            await self.connect_to(str(self.channel.id))
-
-    async def connect_to(self, channel_id: str):
-        """A channel_id of `None` means disconnect."""
-        ws = self.bot._connection._get_websocket(self.guild.id)
-        await ws.voice_state(str(self.guild.id), channel_id)
-        # The above looks dirty, we could alternatively use `bot.shards[shard_id].ws` but that assumes
-        # the bot instance is an AutoShardedBot.
-
-    @commands.command(aliases=['s'])
-    async def setup(self, ctx):
-        """Plays lofi in Lofi Study Room voice channel."""
-        # Get the player for this guild from cache.
-        player = self.bot.lavalink.player_manager.get(ctx.guild.id)
-        # Remove leading and trailing <>. <> may be used to suppress embedding links in Discord.
+    @commands.command()
+    async def play(self, ctx):
         query = "https://www.youtube.com/watch?v=DWcJFNfaw9c"
 
-        # Get the results for the query from Lavalink.
-        results = await player.node.get_tracks(query)
+        tracks = await self.bot.wavelink.get_tracks(f'ytsearch:{query}')
+        
+        if not tracks:
+            return await ctx.send('Could not find any songs with that query.')
 
-        track = results['tracks'][0]
+        player = self.bot.wavelink.get_player(self.guild.id)
+        if not player.is_connected:
+            await ctx.invoke(self.connect_)
 
-        # You can attach additional information to audiotracks through kwargs, however this involves
-        # constructing the AudioTrack class yourself.
-        track = lavalink.models.AudioTrack(track, ctx.author.id, recommended=True)
-        player.add(requester=ctx.author.id, track=track)
-
-        # We don't want to call .play() if the player is playing as that will effectively skip
-        # the current track.
-        if not player.is_playing:
-            await player.play()
-        await ctx.send("Setup complete!")
+        await ctx.send(f'Added {str(tracks[0])} to the queue.')
+        await player.play(tracks[0])
 
 def setup(bot):
     bot.add_cog(Music(bot))
