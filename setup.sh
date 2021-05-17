8617f02bb0c87f05711c8c70d8c33928ee2e0bfc
Joey Yu <itsjoeoui@gmail.com>
2021-05-16 23:14:02 -0400

    Setup script is useless now
    Signed-off-by: Joey Yu <itsjoeoui@gmail.com>

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pkill screen
-git reset --hard
-git pull
-screen -S lavalink -dm bash -c "java -jar Lavalink.jar"
-pipenv sync
-screen -S bot -dm bash -c "pipenv shell 'python3 src/bot.py'"
-screen -ls
